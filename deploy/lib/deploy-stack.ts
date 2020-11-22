f9417e6812b1ba75492cad4d3b74dc85f8443de6
David ROBIN <david.a.robin@gmail.com>
2020-11-22 16:32:11 +0100

    fix path to layer

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -5,7 +5,7 @@ export class FlaskApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const layer = new lambda.LayerVersion(this, 'FlaskLayer', {
-      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist/third-parties-layer.zip')),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist/layer/third-parties-layer.zip')),
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_8],
       license: 'Apache-2.0',
       description: 'Third parties modules layer for the flask graphql app',
