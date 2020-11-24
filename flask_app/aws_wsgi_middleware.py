0b14af2b6af9e9af493e37eca27c4814fbdd201a
David ROBIN <david.a.robin@gmail.com>
2020-11-24 19:19:46 +0100

    linted code

--- flask_app/aws_wsgi_middleware.py
+++ flask_app/aws_wsgi_middleware.py
@@ -3,7 +3,8 @@ from graphql_app import app
 
 
 def proxied_app(environ, start_response):
-    environ['SCRIPT_NAME'] = '/public'
+    environ["SCRIPT_NAME"] = "/public"
     return app(environ, start_response)
+
 
 lambda_handler = make_lambda_handler(proxied_app)
--- flask_app/graphql_app.py
+++ flask_app/graphql_app.py
@@ -1,9 +1,13 @@
 from flask import Flask, url_for
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def home():
     return "hello from flask serverless"
+
+
 @app.route("/page")
 def page():
-    return url_for('static', filename='test.html')
+    return url_for("static", filename="test.html")
